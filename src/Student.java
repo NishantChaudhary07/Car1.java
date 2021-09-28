afd86ddf79cd5d77f37230493cdf7e14ac438e06
Nishant Chaudhary <bohrabadal@gmail.com>
2021-09-28 23:16:29 +0530

    update 4.0.3

--- src/Student.java
+++ src/Student.java
@@ -1,3 +1,4 @@
+/*Q4. Design an application in java that contains a class Student having properties name and percentage. Create a class Test that declares three instances of Student class. The values are passed through constructor at the time of its object creation. The output should be the name and percentage of the topper.*/
 import java.util.Scanner;
 
 public class Student {
